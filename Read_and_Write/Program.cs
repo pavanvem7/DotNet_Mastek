51e77cf6b866ca381f44a09c6ccded0be2c90f31
Pavan <pavanvemula7@gmail.com>
2020-03-19 10:24:11 +0530

    Added Read/Write

---
+++ Read_and_Write/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your first name");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+    }
+}
